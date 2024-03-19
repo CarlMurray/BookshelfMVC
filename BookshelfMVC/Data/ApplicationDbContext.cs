de0413c0ca38f7f523404a7c2c10dc88552719d7
CarlMurray <carl.j.murray@gmail.com>
2024-03-19 19:45:04 +0000

    add blogpost model

--- BookshelfMVC/Data/ApplicationDbContext.cs
+++ BookshelfMVC/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookshelfMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookshelfMVC.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<BlogPostModel> BlogPosts { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ BookshelfMVC/Models/BlogPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookshelfMVC.Models
+{
+    [Table("BlogPosts")]
+    public class BlogPostModel
+    {
+        
+        [Key][Required]public int Id { get; set; }
+        [Required][MaxLength(200)]public string Title { get; set; }
+        [Required][MaxLength(5000)]public string Content { get; set; }
+        [Required]public DateTime Created { get; set; }
+        [Required]public DateTime Updated { get; set; }
+    }
+}
